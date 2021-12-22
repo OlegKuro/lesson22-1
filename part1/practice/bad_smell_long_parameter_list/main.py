fbe592ae838ccd900ab1d7300e34afeaec5e7381
OlegKuro <o.khayatov@skyeng.ru>
2021-12-23 00:50:27 +0300

    badsmell long parameter

--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -1,50 +1,73 @@
-# У нас есть какой-то юнит, которому мы в параметры передаем
-# - наше игровое поле
-# - х координату
-# - у координату
-# - направление смещения
-# - летит ли он
-# - крадется ли он
-# - скорость
-# В этом примере есть сразу несколько запахов плохого кода. Исправьте их
-#   (длинный метод, длинный список параметров)
+class Coords:
+    def __init__(self, coords):
+        self.x, self.y = coords
+
+    def by_delta(self, dx: int = 0, dy: int = 0):
+        return self.__class__((self.x + dx, self.y + dy))
+
+
+class State:
+    CRAWL = 'crawl'
+    FLYING = 'flying'
+    FEET = 'feet'
+
+    def __init__(self, state):
+        self.state = state
+
+
+class Direction:
+    LEFT = 'LEFT'
+    RIGHT = 'RIGHT'
+    UP = 'UP'
+    DOWN = 'DOWN'
+
+    def __init__(self, direction):
+        self.direction = direction
+
+
+class Field:
+    def __init__(self):
+        self.unit = None
+        self.coords = None
+
+    def set_unit(self, unit, coords):
+        self.unit = unit
+        self.coords = coords
+
+
+class UnitMoveRequestDTO:
+    def __init__(self, field: Field, direction: Direction, coords: Coords, state: State = State.FEET, speed: int = 1):
+        self.field = field
+        self.direction = direction
+        self.coords = coords
+        self.state = state
+        self.speed = speed
 
 
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
+    def move(self, request: UnitMoveRequestDTO):
+        delta = self._get_speed_by_state(request.speed, request.state)
+        new_coords = self._get_new_coords(request.coords, delta, request.direction)
+        request.field.set_unit(self, new_coords)
 
-        if is_fly and crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
+    @staticmethod
+    def _get_speed_by_state(original_speed, state):
+        if state == State.CRAWL:
+            return original_speed * 0.5
+        if state == State.FLYING:
+            return original_speed * 1.2
+        if state == State.FEET:
+            return original_speed
+        raise ValueError(f'Unsupported state {state}')
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-
-            field.set_unit(x=new_x, y=new_y, unit=self)
-
-#     ...
+    @staticmethod
+    def _get_new_coords(old_coords: Coords, delta: int, direction: Direction) -> Coords:
+        if direction == Direction.DOWN:
+            return old_coords.by_delta(dy=-delta)
+        if direction == Direction.RIGHT:
+            return old_coords.by_delta(dx=delta)
+        if direction == Direction.UP:
+            return old_coords.by_delta(dy=delta)
+        if direction == Direction.LEFT:
+            return old_coords.by_delta(dx=-1)
+        raise ValueError(f'Unsupported direction {direction}')
