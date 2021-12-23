eac65bfe01be3b502171217a594140134c9c02ef
OlegKuro <o.khayatov@skyeng.ru>
2021-12-23 23:20:34 +0300

    review fixes

--- part1/practice/bad_smell_comments/solution.py
+++ part1/practice/bad_smell_comments/solution.py
@@ -7,33 +7,22 @@ class Unit:
 
         if is_fly:
             speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
+        elif crawl:
             speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
+        if direction == 'UP':
+            new_y = y_coord + speed
+            new_x = x_coord
+        elif direction == 'DOWN':
+            new_y = y_coord - speed
+            new_x = x_coord
+        elif direction == 'LEFT':
+            new_y = y_coord
+            new_x = x_coord - speed
+        elif direction == 'RIGTH':
+            new_y = y_coord
+            new_x = x_coord + speed
+
+        field.set_unit(x=new_x, y=new_y, unit=self)
 
 #     ...
